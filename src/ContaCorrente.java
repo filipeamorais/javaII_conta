3d2dcbe112a975d7add43e7cefd061ecbb61fd8f
Filipe de Andrade Morais <filipe.morais@ul.com>
2017-11-21 11:47:31 -0200

    Mais alterações para o Abstract

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -3,6 +3,7 @@ public class ContaCorrente extends Conta {
 
 	@Override
 	public void atualiza(double taxa) {
-		super.atualiza(taxa * 2);
+		//super.atualiza(taxa * 2);
+		this.saldo+=(this.saldo*taxa*2);
 	}
 }
