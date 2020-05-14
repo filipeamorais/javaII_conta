d4af48659fc7edc4ec39f09b8ad61bfde45cd36b
filipeam <afilipem@gmail.com>
2020-05-13 21:05:15 -0500

    Create interface Tributavel that is implemented on the ContaCorrente and the new class SeguroDeVida. Create TestaTributavel to test the new interface working.

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,9 +1,13 @@
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
 	@Override
 	public void atualiza(double taxa) {
 		//super.atualiza(taxa * 2);
 		this.saldo+=(this.saldo*taxa*2);
+	}
+
+	public double calculaTributos() {
+		return this.getSaldo() * 0.01;
 	}
 }
---
+++ src/SeguroDeVida.java
@@ -0,0 +1,6 @@
+class SeguroDeVida implements Tributavel{
+    
+    public double calculaTributos(){
+        return 42;
+    }
+}
