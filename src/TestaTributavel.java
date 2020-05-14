c793f5217a29f3dc2c5aa996909bb93c977383f3
filipeam <afilipem@gmail.com>
2020-05-13 22:29:01 -0500

    Add the code to create the object ContaCorrente, call the deposita and print the tributos.

--- src/TestaTributavel.java
+++ src/TestaTributavel.java
@@ -1,3 +1,13 @@
 public class TestaTributavel {
     
+    public static void main(String [] args){
+        ContaCorrente cc = new ContaCorrente();
+        cc.deposita(100);
+        System.out.println(cc.calculaTributos());
+
+        // test polimorfism
+        Tributavel t = cc;
+        System.out.println(t.calculaTributos());
+        //System.out.println(t.getSaldo());
+    }
 }
