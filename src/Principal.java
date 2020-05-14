fb532f2d1b165cadf5c97bb12b1ad37829a45bfc
filipeam <afilipem@gmail.com>
2020-05-13 22:51:40 -0500

    Add /n in the printf argument.

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 	public static void main(String[] args) {
 		Conta conta = new ContaCorrente();
 		conta.deposita(100.0);
-		System.out.printf("O saldo é: %.2f", conta.getSaldo());
+		System.out.printf("O saldo é: %.2f\n", conta.getSaldo());
 
 		ContaCorrente contaCorrente = new ContaCorrente();
 	}
