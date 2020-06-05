8ff3a6f64bf6b004a5886bc9d30228d80ba381bc
filipeam <afilipem@gmail.com>
2020-06-04 21:41:11 -0500

    Add the Try and Catch Exception to avoid the program to crash.

--- src/TestaExcecao.java
+++ src/TestaExcecao.java
@@ -1,24 +1,28 @@
 public class TestaExcecao {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("inicio main");
         m1();
         System.out.println("fim main");
 
     }
 
-    private static void m1(){
+    private static void m1() {
         System.out.println("inicio m1");
         m2();
         System.out.println("fim m1");
     }
 
-    private static void m2(){
+    private static void m2() {
         System.out.println("inicio m2");
 
         int[] nums = new int[5];
-        for(int i=0; i<6; i++){
-            nums[i]=i*2;
-            System.out.println(nums[i]);
+        for (int i = 0; i < 6; i++) {
+            try {
+                nums[i] = i * 2;
+                System.out.println(nums[i]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("problema!");
+            }
         }
         System.out.println("fim m2");
     }
